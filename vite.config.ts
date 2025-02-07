34f94beae8ca5e7079ab9b6410122e1937ca1dfa
noy4 <naoyan418@gmail.com>
2025-02-08 02:41:08 +0900

    '/slowly-birds-viewer/',

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/slowly-birds-log/',
+  base: '/slowly-birds-viewer/',
   server: {
     fs: {
       // プロジェクトルートの外のファイルへのアクセスを許可
